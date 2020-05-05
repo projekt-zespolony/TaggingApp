09bd53e791073f54662a8c66cf3032df30ab670a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-05-06 01:56:36 +0200

    Add sensors model

--- Tagging/Model/Sensors.cs
+++ Tagging/Model/Sensors.cs
@@ -8,5 +8,20 @@ namespace Tagging.Model
 {
     public class Sensors
     {
+        private long Timestamp { get; }
+        private float Temperature { get; }
+        private float Pressure { get; }
+        private float Humidity { get; }
+        private float Gas { get; }
+
+        public Sensors(long timestamp, float temperature, float pressure, float humidity, float gas)
+        {
+            Timestamp = timestamp;
+            Temperature = temperature;
+            Pressure = pressure;
+            Humidity = humidity;
+            Gas = gas;
+        }
+
     }
 }
