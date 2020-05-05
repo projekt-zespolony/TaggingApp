2a9133ccf160462e41583d22bff91bd4409d2d50
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-05-05 14:39:12 +0200

    Refactor form name

--- Tagging/Program.cs
+++ Tagging/Program.cs
@@ -16,7 +16,7 @@ namespace Tagging
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
