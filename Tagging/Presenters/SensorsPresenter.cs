e1c6e7463421bffd0fad3fe9564d09d4d8e06002
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-05-07 04:05:20 +0200

    Add SensorsPresenter

---
+++ Tagging/Presenters/SensorsPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tagging.Model;
+using Tagging.View;
+
+namespace Tagging.Presenters
+{
+    public class SensorsPresenter
+    {
+        private MainForm _mainForm;
+        private List<Sensors> _sensorsList;
+
+        public SensorsPresenter(MainForm mainForm)
+        {
+            _mainForm = mainForm;
+            _sensorsList = new List<Sensors>();
+        }
+
+
+    }
+}
